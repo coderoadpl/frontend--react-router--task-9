85710bf4fdd7c8c4e05084750b3208acd42a403b
Mateusz Choma <kontakt@coderoad.pl>
2021-11-09 12:45:27 +0100

    Using `Router` `Routes` and `Route`

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import React from 'react'
 
+import { Routes, Route } from 'react-router-dom'
+
 import PageHome from './pages/PageHome'
 import PageMoviesSearch from './pages/PageMoviesSearch'
 
 export const App = () => {
   return (
     <div>
-      <PageHome />
-      <PageMoviesSearch />
+      <Routes>
+        <Route
+          path={'/'}
+          element={<PageHome />}
+        />
+        <Route
+          path={'/movies/search'}
+          element={<PageMoviesSearch />}
+        />
+      </Routes>
     </div>
   )
 }
